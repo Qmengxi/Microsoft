7ae158da49dd05ddb447a2629ad55c6263bb204a
mengxiaoxi <8199082332qq.com>
2016-10-23 22:31:34 +0800

    banner

--- js/Microsoft.js
+++ js/Microsoft.js
@@ -1,3 +1,20 @@
 $(function(){
-	
+	var timer=setInterval(move,5000);
+	function move(){
+		console.log(1)
+		$(".banner div").stop().fadeToggle("fast");
+		$(".banner div article p").css({"animation": "name 2s linear"})
+	}
+	$(".banner").mouseenter(function(){
+		console.log(1)
+		clearInterval(timer);
+		$(".banner").on("mousedown",".iconfont",function(){
+			$(this).parent().hide();
+			$(this).parent().siblings().show()
+		})
+	})
+	$(".banner").mouseleave(function(){
+		console.log(2)
+		timer=setInterval(move,5000);
+	})
 })
